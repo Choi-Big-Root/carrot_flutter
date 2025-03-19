0e40488b45ebe11b2089799177f63eb509645192
Choi-Big-Root <bigroot0601@gmail.com>
2025-03-19 23:21:53 +0900

    [20250319] NameRoute 를 사용하여 화면이동을 연습

--- lib/src/screen/intro.dart
+++ lib/src/screen/intro.dart
@@ -1,3 +1,4 @@
+import 'package:carrot_flutter/src/screen/auth/register.dart';
 import 'package:flutter/material.dart';
 
 class Intro extends StatelessWidget {
@@ -5,6 +6,29 @@ class Intro extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'Flutter +Node.js 로 SNS 중고 거래 애플리케이션을 작성.',
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/register');
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => const Register()),
+                // );
+              },
+              child: const Text('사용하러 가기'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
